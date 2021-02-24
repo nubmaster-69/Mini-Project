373bb340e52398a131f06edfc98a42ad6ac3805d
nubmaster-69 <chihieunguyen9a2@gmail.com>
2021-02-24 08:41:24 +0700

    Add sign up form and script to switch between 2 forms

--- js/login.js
+++ js/login.js
@@ -1,10 +1,28 @@
-function showPassword() {
-    if (document.getElementById('pass').type == "password") {
-        document.getElementById('pass').type = "text"
-        document.getElementById('pass-icon').className = "fas fa-eye";
+function showPasswordSignIn() {
+    if (document.getElementById('sign-in').type == "password") {
+        document.getElementById('sign-in').type = "text"
+        document.getElementById('sign-in-icon').className = "fas fa-eye";
     }
     else {
-        document.getElementById('pass').type = "password";
-        document.getElementById('pass-icon').className = "fas fa-eye-slash"
+        document.getElementById('sign-in').type = "password";
+        document.getElementById('sign-in-icon').className = "fas fa-eye-slash"
     }
+}
+
+function showPasswordSignUp() {
+    if (document.getElementById('sign-up').type == "password") {
+        document.getElementById('sign-up').type = "text"
+        document.getElementById('sign-up-icon').className = "fas fa-eye";
+    }
+    else {
+        document.getElementById('sign-up').type = "password";
+        document.getElementById('sign-up-icon').className = "fas fa-eye-slash"
+    }
+}
+
+function formToggle() {
+    if (document.getElementById('check').checked == true)
+        document.getElementById('check').checked = false;
+    else
+        document.getElementById('check').checked = true;
 }
