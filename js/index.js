791a2ff6771cb76df9e076b02da661e2e51ac22d
nubmaster-69 <chihieunguyen9a2@gmail.com>
2021-03-04 12:43:44 +0700

    Change navbar toggle from pure css to javascript, change autoSlide method, add method to add item(s) to favorite list

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
-// var idx = 1;
-// setInterval(function autoSlide() {
+let images = [
+    {
+        imgSrc: "./img/products/img-slider/1.png",
+        bgColor: "#ff3e00"
+    },
+    {
+        imgSrc: "./img/products/img-slider/2.png",
+        bgColor: "#766bd1"
+    },
+    {
+        imgSrc: "./img/products/img-slider/3.png",
+        bgColor: "#2c6ad9"
+    },
+    {
+        imgSrc: "./img/products/img-slider/4.png",
+        bgColor: "#ff6700"
+    },
+    {
+        imgSrc: "./img/products/img-slider/5.png",
+        bgColor: "#e70213"
+    }
+];
 
-//     if (idx > 5) {
-//         document.getElementById('showcase-img').src = "./img/products-pic/img-slider/" +
-//             (1) + ".png";
-//         idx = 2;
+// let idx = 0, imagesLength = images.length;
+// setInterval(function autoSlide() {
+//     idx++;
+//     if (idx >= imagesLength) {
+//         idx = 0;
+//         AutoChangeImg(images[idx].imgSrc);
+//         changeColor(images[idx].bgColor);
 //     }
 //     else {
-//         document.getElementById('showcase-img').src = "./img/products-pic/img-slider/" +
-//             (idx++) + ".png";
+//         AutoChangeImg(images[idx].imgSrc);
+//         changeColor(images[idx].bgColor);
 //     }
-// }, 5000);
+// }, 6000);
+
+function AutoChangeImg(source) {
+    document.getElementById('showcase-img').src = source;
+}
 
 function changeImg(source) {
     document.getElementById('showcase-img').src = source.src;
@@ -19,3 +46,22 @@ function changeImg(source) {
 function changeColor(newColor) {
     document.documentElement.style.setProperty('--primary-color', newColor);
 }
+
+
+const menuToggler = document.querySelector('.menu-toggler');
+const navLinks = document.querySelector('.nav-links');
+menuToggler.addEventListener('click', () => {
+    navLinks.classList.toggle('menu-active');
+    menuToggler.classList.toggle('menu-close');
+});
+
+// Add item(s) to favorite list
+const toFavList = document.querySelectorAll('.to-fav-list')
+toFavList.forEach(item => {
+    item.addEventListener('click', () => {
+        if (item.classList.contains('far'))
+            item.classList.replace('far', 'fas');
+        else
+            item.classList.replace('fas', 'far');
+    });
+});
